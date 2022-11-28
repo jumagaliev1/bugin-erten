61ea24e9039dd8d44a36809e9a00fdf33c88cf66
Magzhan Ermekbay <magzhan.ermekbay@gmail.com>
2022-11-28 22:18:32 +0600

    settings preferences

--- app/src/main/java/com/example/bugin_erten/SettingsFragment.kt
+++ app/src/main/java/com/example/bugin_erten/SettingsFragment.kt
@@ -1,18 +1,11 @@
 package com.example.bugin_erten
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.preference.PreferenceFragmentCompat
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
     }
 }
